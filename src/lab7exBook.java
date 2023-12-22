a48886dcf9d41bfc85dc74515f087232b3863e0c
4honeymoon <mkhutinaeva@bk.ru>
2023-12-22 12:09:16 +0300

    Initial commit

---
+++ src/lab7exBook.java
@@ -0,0 +1,20 @@
+public class Book implements Printable{
+    private String title;
+
+    public Book(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public void print() {
+        System.out.println("Книга: " + title);
+    }
+
+    public static void printBooks(Printable[] printable){
+        for(Printable item : printable){
+            if(item instanceof Book){
+                item.print();
+            }
+        }
+    }
+}
